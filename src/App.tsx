1d9511758b6b7ca03f9bba700db1af72e6cf63ec
Laurent Mignot <laurent@shapedigital.io>
2022-08-30 22:04:14 +0200

    feat: Add Home view

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,12 @@
-import './App.css';
+import Home from './views/Home';
+import { SearchContextProvider } from './context/SearchContext';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <SearchContextProvider>
+        <Home />
+      </SearchContextProvider>
     </div>
   );
 }
---
+++ src/views/Home.tsx
@@ -0,0 +1,14 @@
+// import { useSearchContext } from '../context/SearchContext';
+
+
+function Home() {
+  // const {isLoading, isError, isSuccess, data, error } = useSearchContext();
+
+  return (
+    <>
+      <h1>Home</h1>
+    </>
+  )
+}
+
+export default Home;
